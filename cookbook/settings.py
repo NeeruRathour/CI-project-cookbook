9c53defe8dcb6e3bb93959293c290a5e838dc4f2
Neeru Rathour <Neerurathour@gmail.com>
2024-07-28 10:26:00 +0000

    deployment changes

--- cookbook/settings.py
+++ cookbook/settings.py
@@ -23,8 +23,8 @@ if os.path.exists('env.py'):
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = os.environ.get('SECRET_KEY')
-SECRET_KEY = 'H4mT4m'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
